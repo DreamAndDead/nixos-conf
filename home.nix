64266d8bbdc35e7e89f2d9825437db1f8a667577
David Zhang <aquairain@gmail.com>
2025-10-13 15:44:57 +0800

    update git config files

--- home.nix
+++ home.nix
@@ -4,14 +4,13 @@
   home.username = "david";
   home.homeDirectory = "/home/david";
   home.stateVersion = "25.05";
-  programs.git.enable = true;
+  programs.git = {
+    enable = true;
+    userEmail = "aquairain@gmail.com";
+    userName = "David Zhang";
+  };
   programs.fish = {
     enable = true;
-    #profileExtra = ''
-    #  if [ -z "$WAYLAND_DISPLAY" ] && [ "$XDG_VTNR" = 1 ]; then
-    #    exec uwsm start -S hyprland-uwsm.desktop
-    #  fi
-    #'';
   };
 }
 
