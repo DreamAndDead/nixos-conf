39e2336e5036d68dc0bd205f5c40c7d0b8c9ea98
David Zhang <aquairain@gmail.com>
2025-10-21 00:19:26 +0800

    after test, burn into boot

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -31,17 +31,17 @@
     fsType = "ext4";
   };
 
-  #  fileSystems."/home/david/Windows" = {
-  #    depends = [
-  #      "/"
-  #    ];
-  #    device = "/dev/disk/by-uuid/0292D9CF92D9C777";
-  #    fsType = "ntfs";
-  #    options = [
-  #      "gid=100"
-  #      "uid=1000"
-  #    ];
-  #  };
+  fileSystems."/home/david/Windows" = {
+    #    depends = [
+    #      "/"
+    #    ];
+    device = "/dev/disk/by-uuid/0292D9CF92D9C777";
+    fsType = "ntfs";
+    #    options = [
+    #      "gid=100"
+    #      "uid=1000"
+    #    ];
+  };
 
   swapDevices = [ ];
 
